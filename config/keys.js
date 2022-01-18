fcfcbf609bdf569cf1ac45b554a088eedeafffdc
Faheim Arslan M <faheimarslan@gmail.com>
2022-01-18 09:41:13 +0000

    added prod mongodb uri

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "prod",
+    mongoURI: "mongodb+srv://tcdase6:tcdase6@cluster0.ybqis.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     devMongoURI: "dev"
 };
