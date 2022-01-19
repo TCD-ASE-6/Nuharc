1331b29e243d1d61618201a00b7f69bf5bb7e3d9
Faheim Arslan M <faheimarslan@gmail.com>
2022-01-19 10:33:05 +0000

    added redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    </Provider>
   );
 }
 
