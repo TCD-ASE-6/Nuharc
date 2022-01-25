78f17fb22c07a35d67bb09b2efac75fe9f52e9cc
bhushan-borole <borolebhushan8@gmail.com>
2022-01-25 15:26:34 +0000

    add getRoute api

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,9 +1,15 @@
 import React, { Component, useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
 import Marker from "./Marker";
+import keys from "../../../../config/keys";
 
 function Map() {
-  const [currentLocation, setCurrentLocation] = useState(0);
+  const [currentCoordinates, setCurrentCoordinates] = useState(0);
+  const [destinationCoordinates, setDestinationCoordinates] = useState(0);
+  const [allRoutes, setAllRoutes] = useState(0);
+  const [currentRoute, setCurrentRoute] = useState(0);
+  const [currentRouteIndex, setCurrentRouteIndex] = useState(0);
+  const [countOfRoutes, setCountOfRoutes] = useState(0);
   const [zoom, setZoom] = useState(15);
 
   // Getting the current location
@@ -13,12 +19,34 @@ function Map() {
       lat: position.coords.latitude,
       lng: position.coords.longitude,
     });
+
+    setDestinationCoordinates({
+      lat: 53.3458095,
+      lng: -6.2543664,
+    });
   });
+
+  getRoutes = async (from, to) => {
+    let data = "";
+    try {
+      data = await fetch(
+        `http://localhost:8080/api/directions/getRoute?source=${from}&destination=${to}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+    data = await data.text().then((data) => JSON.parse(data));
+    if (data.status === "OK" && data.routes.length) {
+      setAllRoutes(data);
+      setCountOfRoutes(data.length);
+      setCurrentRouteIndex(0);
+    }
+  };
 
   return (
     <div style={{ height: "93.5vh", width: "100%" }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyAK7fU7K5MEJieLeb91s-1ujV87tcUp6VY" }}
+        bootstrapURLKeys={{ key: keys.googleMapsAPIKey }}
         center={currentLocation}
         defaultZoom={zoom}
       >
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const directions = require("./routes/api/directions");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const db = (process.env.NODE_ENV.trim() === "production") 
-    ? require('./config/keys').mongoURI 
-    : require('./config/keys').devMongoURI;
+const db =
+  process.env.NODE_ENV.trim() === "production"
+    ? require("./config/keys").mongoURI
+    : require("./config/keys").devMongoURI;
 
 mongoose
-    .connect(db)
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error(err));
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error(err));
+
+app.use("/api/directions", directions);
 
 const port = process.env.PORT || 8080;
 
