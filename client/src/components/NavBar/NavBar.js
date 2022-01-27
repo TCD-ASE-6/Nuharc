03f9089d35de0a8d28412e1fd9c16cfc546eeb54
arslant-tcd <arslant@tcd.ie>
2022-01-27 16:33:45 +0000

    created nav elements for login signup and links

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
   Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
   Nav,
   NavItem,
   NavLink,
-  NavbarText,
-} from "reactstrap";
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -33,16 +38,33 @@ class NavBar extends React.Component {
           <NavbarToggler onClick={this.toggleNavbar} />
           <Collapse isOpen={this.state.collapsed} navbar>
             <Nav className="me-auto" navbar>
+              <Link to="/test">Test</Link>
               <NavItem>
                 <NavLink href="#">Map</NavLink>
               </NavItem>
               <NavItem>
                 <NavLink href="#">Report Disaster</NavLink>
               </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Account
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem href="/login">
+                  Log in
+                </DropdownItem>
+                <DropdownItem href="/signup">
+                  Sign up
+                </DropdownItem>
+                <DropdownItem>
+                  Test
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
             </Nav>
             <NavbarText>Profile Avatar</NavbarText>
           </Collapse>
-        </Navbar>
+        </Navbar>  
       </div>
     );
   }
