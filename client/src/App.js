3dbc450cd5b7422887f8f599365f78de5f2d196c
Shubham Uniyal <shubham.uniyal@gmail.com>
2022-02-17 15:59:45 +0000

    Added map2 component

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import store from "./store";
 import NavBar from './components/NavBar/NavBar';
 import LoginPage from "./components/LoginPage/LoginPage";
 import SignUp from "./components/RegisterPage/SignUp";
+import Map2 from "./components/Map/Map2";
 import Map from "./components/Map/Map";
 import ReportIncident from "./components/ReportIncident/ReportIncident";
 
@@ -20,8 +21,8 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route exact path='/' exact element={<Map />} />
-          <Route path='/map' element={<Map />} />
+          <Route exact path='/' exact element={<Map2 />} />
+          <Route path='/map' element={<Map2 />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/report' element={<ReportIncident />} />
--- client/src/components/Map/Map2.js
+++ client/src/components/Map/Map2.js
@@ -1,4 +1,8 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import Marker from "./Marker";
+import IncidentMarkerList from "./IncidentMarkerList";
+
 const API_KEY = "AIzaSyAK7fU7K5MEJieLeb91s-1ujV87tcUp6VY";
 /* global google */
 
@@ -20,44 +24,19 @@ class Map2 extends React.Component {
     return path;
   }
 
-  componentDidMount() {
-    const [currentCoordinates, setCurrentCoordinates] = useState(0);
-    const [destinationCoordinates, setDestinationCoordinates] = useState(0);
-    const [allRoutes, setAllRoutes] = useState(0);
-    const [currentRoute, setCurrentRoute] = useState(0);
-    const [currentRouteIndex, setCurrentRouteIndex] = useState(0);
-    const [countOfRoutes, setCountOfRoutes] = useState(0);
-    const [zoom, setZoom] = useState(15);
-    const [showDirections, setShowDirections] = useState(false);
-    const [getRoutesClicked, setGetRoutesCliked] = useState(false);
-    const [map, setMap] = useState(null);
-    const [maps, setMaps] = useState(null);
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCoordinates: {
+        lat: 53.36460864423722,
+        lng: -6.256456570972608,
+      },
+      destinationCoordinates: {
+        lat: 53.35600864423722,
+        lng: -6.256456570972608
+      } 
+    }
     this.setCurrentPostion();
-  }
-
-  setCurrentPostion() {
-    // Getting the current location
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000
-    };
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setCurrentCoordinates({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-        setDestinationCoordinates({
-          lat: 53.35460864423722,
-          lng: -6.256456570972608,
-        });
-      },
-      function error(err) {
-        console.log(err)
-        console.error(`ERROR(${err.code}): ${err.message}`);
-      },
-      options
-    );
   }
 
   async getRoutes(from, to) {
@@ -69,57 +48,71 @@ class Map2 extends React.Component {
     } catch (error) {
       console.log(error);
     }
-    data = JSON.parse(await data.text())
-    setAllRoutes(data);
-    setCountOfRoutes(data.length);
-    setCurrentRouteIndex(0);
-    setCurrentRoute(data[currentRouteIndex]);
+    data = await JSON.parse(data.text())
+    // let polyline = new maps.Polyline({
+    //   path: getPolyLineFromRoute(currentRoute),
+    //   geodesic: true,
+    //   strokeColor: "#00a1e1",
+    //   strokeOpacity: 1.0,
+    //   strokeWeight: 4,
+    // });
+  }
 
-    let polyline = new maps.Polyline({
-      path: getPolyLineFromRoute(currentRoute),
-      geodesic: true,
-      strokeColor: "#00a1e1",
-      strokeOpacity: 1.0,
-      strokeWeight: 4,
-    });
-
-    polyline.setMap(map);
+  setCurrentPostion() {
+     // Getting the current location
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+    navigator.geolocation.watchPosition(
+      (position) => {
+        this.setState({currentCoordinates: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }})
+      },
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      },
+      options
+    );
   }
 
   render() {
     return (
       <div style={{ height: "93.5vh", width: "100%" }}>
-        <Autocomplete
-          onPlaceSelected={(place) => console.log(place)}
-        />
         <button
           onClick={() =>
-            getRoutes(
-              get_str(currentCoordinates),
-              get_str(destinationCoordinates)
+            this.getRoutes(
+              this.get_str(this.state.currentCoordinates),
+              this.get_str(this.state.destinationCoordinates)
             )
           }
         >
           Get Routes
         </button>
         <GoogleMapReact
-          center={currentCoordinates}
-          defaultZoom={zoom}
+          center={this.state.currentCoordinates}
+          defaultZoom={15}
+          bootstrapURLKeys={{ key: API_KEY }}
+          center={this.state.currentCoordinates}
           yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map, maps }) => setMapState(map, maps)}
         >
           <Marker
-            lat={currentCoordinates.lat}
-            lng={currentCoordinates.lng}
+            lat={this.state.currentCoordinates.lat}
+            lng={this.state.currentCoordinates.lng}
             name="Current Location"
             color="blue"
           ></Marker>
           <Marker
-            lat={destinationCoordinates.lat}
-            lng={destinationCoordinates.lng}
+            lat={this.state.destinationCoordinates.lat}
+            lng={this.state.destinationCoordinates.lng}
             name="Destination Location"
             color="red"
           ></Marker>
+          <IncidentMarkerList>
+          </IncidentMarkerList>
         </GoogleMapReact>
       </div>
     );
