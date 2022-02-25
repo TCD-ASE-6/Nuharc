fff56b2e40d4a86d7ba22251dec861a17e505f94
Bhushan Borole <borolebhushan8@gmail.com>
2022-02-25 17:33:00 +0000

    removed imports

--- client/src/App.js
+++ client/src/App.js
@@ -2,35 +2,31 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import store from "./store";
-import NavBar from './components/NavBar/NavBar';
+import NavBar from "./components/NavBar/NavBar";
 import LoginPage from "./components/LoginPage/LoginPage";
 import SignUp from "./components/RegisterPage/SignUp";
 import Map2 from "./components/Map/Map2";
-import Map from "./components/Map/Map";
 import ReportIncident from "./components/ReportIncident/ReportIncident";
 
-
 function App() {
-
   return (
     <Provider store={store}>
       <Router>
         <NavBar />
         <Routes>
-          <Route exact path='/' exact element={<Map2 />} />
-          <Route path='/map' element={<Map />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/report' element={<ReportIncident />} />
+          <Route exact path="/" exact element={<Map2 />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/report" element={<ReportIncident />} />
         </Routes>
       </Router>
     </Provider>
   );
 }
-
 
 export default App;
