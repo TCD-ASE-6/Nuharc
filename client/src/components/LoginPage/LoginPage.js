51a3276e17e66a3a08228a21ad146c4b4234ed54
Faheim Arslan M <faheimarslan@gmail.com>
2022-04-01 22:01:52 +0100

    reworked login functionality

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
-  let [email, setEmail] = useState("");
-  let [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [hasLoggedIn, setHasLoggedIn] = useState(false);
+  const [didLoginFail, setDidLoginFail] = useState(false);
 
+  // this hook deals with setting a global cookie for user details
+  // contains all information about logged in user like jwt token, email, role, etc.
+  const [cookies, setCookie] = useCookies(["userDetails"]);
+
+  const navigate = useNavigate();
+
+  // create a user object and pass it to the login and store user cookie function
   const loginUser = (e) => {
     e.preventDefault();
     const user = {
@@ -15,48 +26,70 @@ function LoginPage() {
     loginUserAndStoreInCookie(user);
   };
 
+  // function to login user and store the cookie.
   const loginUserAndStoreInCookie = async (user) => {
-    axios.post(`/api/users/login`, user).then((response) => {
-      const userDetails = response.json();
-      // TODO: set global cookies for user details to be checked in everypage
-      // userDetails.
-    });
+    axios
+      .post(`/api/users/login`, user)
+      .then((response) => {
+        // const userDetails = response.json();
+        // store global user details in a cookie
+        setCookie("userDetails", JSON.stringify(response.data), { path: "/" });
+        setHasLoggedIn(true);
+        setDidLoginFail(false);
+        setTimeout(() => {
+          navigate("/");
+        }, 2000);
+      })
+      .catch((err) => {
+        setDidLoginFail(true);
+      });
   };
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
-    // this.setState({ [e.target.name]: e.target.value });
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
-    // this.setState({ [e.target.name]: e.target.value });
   };
 
   return (
-    <Form>
-      <FormGroup>
-        <Label for="userEMail">Email</Label>
-        <Input
-          type="email"
-          name="email"
-          id="userEMail"
-          onChange={(e) => handleEmailChange(e)}
-          placeholder="Enter your Email"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="userPassword">Password</Label>
-        <Input
-          type="password"
-          name="password"
-          id="userPassword"
-          onChange={(e) => handlePasswordChange(e)}
-          placeholder="Enter your password"
-        />
-      </FormGroup>
-      <Button onClick={(e) => loginUser(e)}>Login</Button>
-    </Form>
+    <>
+      <Form>
+        <FormGroup>
+          <Label for="userEMail">Email</Label>
+          <Input
+            type="email"
+            name="email"
+            id="userEMail"
+            onChange={(e) => handleEmailChange(e)}
+            placeholder="Enter your Email"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="userPassword">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="userPassword"
+            onChange={(e) => handlePasswordChange(e)}
+            placeholder="Enter your password"
+          />
+        </FormGroup>
+        <Button onClick={(e) => loginUser(e)}>Login</Button>
+      </Form>
+
+      {hasLoggedIn && (
+        <h4 style={{ color: "green", textAlign: "center" }}>
+          Login Successful.
+        </h4>
+      )}
+      {didLoginFail && (
+        <h4 style={{ color: "red", textAlign: "center" }}>
+          Login Failed.
+        </h4>
+      )}
+    </>
   );
 }
 
