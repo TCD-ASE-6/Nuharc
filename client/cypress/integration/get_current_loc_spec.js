61a765132fbf27a04f5953068bf413c692ca2d7e
YashShukla025 <yashshukla048@gmail.com>
2022-04-01 18:52:38 +0100

    added test case to find current location

--- client/cypress/integration/get_current_loc_spec.js
+++ client/cypress/integration/get_current_loc_spec.js
@@ -1,7 +1,7 @@
 describe("Current Location Functionality", () => {
     it("Log in", async () => {
         cy.visit('/');
-               
+        cy.findByText("Find Current Location").click();
         await cy.findByRole('button', { name: /Find Current Location/i }).click()
             .then((res) => {
                 console.log(res);
