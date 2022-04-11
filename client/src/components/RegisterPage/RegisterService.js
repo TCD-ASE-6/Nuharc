b5aaa4d01f74b639e08a90c4e8306d1fce6f4a57
Rajpal Singh <srajpal206@gmail.com>
2022-04-11 14:28:28 +0100

    style-components for styling

--- client/src/components/RegisterPage/RegisterService.js
+++ client/src/components/RegisterPage/RegisterService.js
@@ -1,8 +1,71 @@
 import React, { useState } from "react";
-import { Button, Form, FormGroup, Label, Input, Container } from "reactstrap";
 import { signupUser } from "../../actions/userActions";
 import Role from "../../helpers/role";
 import { useNavigate } from "react-router-dom";
+import styled, { createGlobalStyle, css } from "styled-components";
+
+const Globalstyle = createGlobalStyle`
+html {
+  height: 100%;
+}
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  background: linear-gradient(to bottom, #49c49d, #e1eec3);
+  height: 100%;
+  margin: 0;
+  color: #555;
+}
+`;
+
+const SharedStyle = css`
+  background-color: #eee;
+  height: 40px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  margin: 10px 0 20px 0;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+const StyleFormedWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 0 20px;
+`;
+
+const StyleForm = styled.form`
+  width: 100%;
+  max-width: 700px;
+  padding: 40px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-sizing: border-box;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
+`;
+
+const StyledInput = styled.input`
+  display: block;
+  width: 100%;
+  ${SharedStyle}
+`;
+
+const StyledButton = styled.button`
+  display: block;
+  background-color: #f7797d;
+  color: #fff;
+  font-size: 0.9rem;
+  border: 0;
+  border-radius: 5px;
+  height: 40px;
+  padding: 0 20px;
+  cursor: pointer;
+  box-sizing: border-box;
+  :hover {
+    background-color: red;
+  }
+`;
 
 export default function RegisterService() {
   const navigate = useNavigate();
@@ -44,65 +107,70 @@ export default function RegisterService() {
   };
 
   return (
-    <Container>
-      <Form onSubmit={submitHandler}>
-        <FormGroup>
-          <Label for="userName">Name</Label>
-          <Input
-            type="text"
+    <>
+      <Globalstyle />
+      <StyleFormedWrapper>
+        <StyleForm onSubmit={submitHandler}>
+          <div>
+            <label htmlFor="userName">Name</label>
+            <StyledInput
+              type="text"
             name="name"
             id="userName"
             onChange={nameChangeHandler}
             value={enteredName}
             placeholder="Enter your Name"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="userSurname">Surname</Label>
-          <Input
-            type="text"
+            />
+          </div>
+          <div>
+            <label htmlFor="userSurname">Surname</label>
+            <StyledInput
+              type="text"
             name="surname"
             id="userSurname"
             onChange={surnameChangeHandler}
             value={enteredSurname}
             placeholder="Enter your Surname"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="userEMail">Email</Label>
-          <Input
-            type="email"
+            />
+          </div>
+          <div>
+            <label htmlFor="userEMail">Email</label>
+            <StyledInput
+              type="email"
             name="email"
             id="userEMail"
             onChange={emailChangeHandler}
             value={enteredEmail}
             placeholder="Enter your Email"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="userPassword1">Enter your Password</Label>
-          <Input
-            type="password"
+            />
+          </div>
+          <div>
+            <label htmlFor="userPassword1">Password</label>
+            <StyledInput
+              type="password"
             name="password1"
             id="userPassword1"
             onChange={password1ChangeHandler}
             value={enteredPassword1}
             placeholder="Enter your Password"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="userPassword2">Confirm your Password</Label>
-          <Input
-            type="password"
+            />
+          </div>
+          <div>
+            <label htmlFor="userPassword2">Confirm Pasword</label>
+            <StyledInput
+              type="password"
             name="password2"
             id="userPassword2"
             onChange={password2ChangeHandler}
             value={enteredPassword2}
             placeholder="Enter your Password Again"
-          />
-        </FormGroup>
-        <Button type="Submit">Sign Up</Button>
-      </Form>
-    </Container>
+            />
+          </div>
+          <div>
+            <StyledButton type="submit">Register</StyledButton>
+          </div>
+        </StyleForm>
+      </StyleFormedWrapper>
+    </>
   );
 }
