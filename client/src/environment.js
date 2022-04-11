e1aacce9756e817977e6b797ea46ed95cfb1665a
Shubham Uniyal <shubham.uniyal@gmail.com>
2022-04-11 20:36:44 +0100

    revert change for production protocol

--- client/src/environment.js
+++ client/src/environment.js
@@ -1,2 +1,2 @@
-const API_URL =  process.env.NODE_ENV.trim() === "production" ? "https://20.93.154.247:8080" : "http://localhost:8080"
+const API_URL =  process.env.NODE_ENV.trim() === "production" ? "http://20.93.154.247:8080" : "http://localhost:8080"
 export default API_URL
