7375bc486e8b88164a8c7d80aeb564b5944f1727
YashShukla025 <yashshukla048@gmail.com>
2022-04-11 12:13:09 +0100

    Added Test case

--- client/cypress/integration/getroute_spec.js
+++ client/cypress/integration/getroute_spec.js
@@ -1,10 +1,11 @@
-describe("Log in Functionality", () => {
-    it("Log in", async () => {
+describe("Calculate route", () => {
+    it("Calculate route", async () => {
         cy.visit('/');
-        cy.findByPlaceholderText("Enter Destination").type('Dublin');
+        cy.findByPlaceholderText("Enter Location").type('Dublin');
+        cy.wait(500);
         // cy.get('ul').closest('strong').click();
         cy.get('.autosuggestElement').contains('Ireland, County Dublin').click();
-        cy.get('.autosuggestElement').contains('Ireland, County Dublin').click();
+        // cy.get('.autosuggestElement').contains('Ireland, County Dublin').click();
         cy.findByText('Calculate Route').click();
         await cy.findByRole('button', { name: /Calculate Route/i }).click()
             .then((res) => {
